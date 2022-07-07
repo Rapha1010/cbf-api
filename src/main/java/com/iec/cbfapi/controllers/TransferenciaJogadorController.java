2e36f19c500eb7f4fbce793d951e078d7537c9a9
Raphael Alves Tavares Leao <raphael.leao@ibm.com>
2022-07-07 18:48:23 -0300

    transferencia de jogador

--- src/main/java/com/iec/cbfapi/controllers/TransferenciaJogadorController.java
+++ src/main/java/com/iec/cbfapi/controllers/TransferenciaJogadorController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,12 @@ public class TransferenciaJogadorController {
 	public ResponseEntity<TransferenciaJogador> findById(@PathVariable Long id) {
 		TransferenciaJogador obj = tjs.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TransferenciaJogador> insert(@RequestBody TransferenciaJogador obj) {
+		TransferenciaJogador transf = tjs.insert(obj);
+		return ResponseEntity.ok().body(transf);
 	}
 
 }
--- src/main/java/com/iec/cbfapi/services/TransferenciaJogadorService.java
+++ src/main/java/com/iec/cbfapi/services/TransferenciaJogadorService.java
@@ -1,10 +1,13 @@
 package com.iec.cbfapi.services;
 
+import com.iec.cbfapi.entities.Jogador;
 import com.iec.cbfapi.entities.TransferenciaJogador;
+import com.iec.cbfapi.repositories.JogadorRepository;
 import com.iec.cbfapi.repositories.TransferenciaJogadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +17,12 @@ public class TransferenciaJogadorService {
 
     @Autowired
     private TransferenciaJogadorRepository transferenciaJogadorRepository;
+    
+    @Autowired
+    private JogadorRepository jogadorRepository;
+    
+    @Autowired
+    private JogadorService jogadorService;
 
     @GetMapping
     public List<TransferenciaJogador> findAll() {
@@ -23,6 +32,20 @@ public class TransferenciaJogadorService {
     public TransferenciaJogador findById(Long id) {
         Optional<TransferenciaJogador> obj = transferenciaJogadorRepository.findById(id);
         return obj.get();
+    }
+    
+    @PostMapping
+    public TransferenciaJogador insert(TransferenciaJogador obj) {
+    	
+    	Jogador jogador = jogadorRepository.getReferenceById(obj.getJogador().getId());
+    	updateData(jogador,obj);
+    	jogadorService.update(jogador);
+    	
+    	return transferenciaJogadorRepository.save(obj);
+    }
+    
+    private void updateData(Jogador jogador,TransferenciaJogador obj) {
+    	jogador.setTime(obj.getTimeDestino());
     }
 
 }
